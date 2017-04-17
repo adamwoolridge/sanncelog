579843f53f8146175ed00bed0a8fcd7461e263c2
Adam Woolridge <a.woolridge@gmail.com>
2017-04-17 21:36:32 +0100

    Initial

---
+++ Assets/Scripts/LogParser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public static class LogParser
+{
+    public static void Test()
+    {
+        string path = Application.dataPath;       
+        path += "/../";
+
+        Debug.Log("Exe path: " + path);
+
+        string [] fileEntries = Directory.GetFiles(path);
+        foreach (string fileName in fileEntries)
+            Debug.Log(fileName);
+
+        
+    }
+}
+
+
+
