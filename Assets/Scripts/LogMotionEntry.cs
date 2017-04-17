d0cc3a5067411c7d41f54553faa03b06545a4a94
Adam Woolridge <a.woolridge@gmail.com>
2017-04-17 22:30:53 +0100

    basic motion entry parsing

---
+++ Assets/Scripts/LogMotionEntry.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogMotionEntry
+{
+    public string startTimeDate;
+    public string endTimeDate;
+    public int channel;
+}
--- Assets/Scripts/LogParser.cs
+++ Assets/Scripts/LogParser.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System;
+using System.Linq;
 
 public static class LogParser
 {
@@ -12,11 +14,45 @@ public static class LogParser
 
         Debug.Log("Exe path: " + path);
 
-        string [] fileEntries = Directory.GetFiles(path);
-        foreach (string fileName in fileEntries)
-            Debug.Log(fileName);
+        string[] files = System.IO.Directory.GetFiles(path, "*.txt");
 
-        
+        Dictionary<int, LogMotionEntry> openChannelLogs = new Dictionary<int, LogMotionEntry>();
+
+        foreach (string fileName in files)
+        {
+            StreamReader reader = File.OpenText(fileName);
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                List<string> splitStrings = line.Split(new[]{"  "}, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToList();
+
+                if (splitStrings[2] == "Alarm Start")
+                {
+                    int channel = int.Parse((splitStrings[3])[splitStrings[3].Length-1].ToString());
+                    LogMotionEntry lme;
+
+                    if (!openChannelLogs.TryGetValue(channel, out lme))
+                    {
+                        lme = new LogMotionEntry();
+                        lme.startTimeDate = splitStrings[1];
+                        lme.channel = channel;
+                        openChannelLogs.Add(channel, lme);
+                    }
+                }
+                else if (splitStrings[2] == "Alarm Stop")
+                {
+                    int channel = int.Parse((splitStrings[3])[splitStrings[3].Length-1].ToString());
+                    LogMotionEntry lme;
+
+                    if (openChannelLogs.TryGetValue(channel, out lme))
+                    {
+                        lme.endTimeDate = splitStrings[1];
+                        Debug.Log(lme.channel + ": " + lme.startTimeDate + "-" + lme.endTimeDate);
+                    }
+                    
+                }
+            }
+        }
     }
 }
 
