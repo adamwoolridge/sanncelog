775b054702328be73fb9efdaa1a036e48238fecf
Adam Woolridge <a.woolridge@gmail.com>
2017-04-18 07:49:28 +0100

    added timeline

---
+++ Assets/Scripts/UI/Daily/UITimeSpanHeader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class UITimeSpanHeader : MonoBehaviour
+{
+    public Transform PrefabTimeHeaderBlock;    
+    public Transform TimeBlockParent;
+    
+    public void Start()
+    {        
+        for (int i=0; i<24; i++)
+        {
+            Transform tb = Instantiate(PrefabTimeHeaderBlock);
+            tb.SetParent(TimeBlockParent, false);
+            tb.GetComponent<UITimeSpanHeaderBlock>().TextTime.text = i.ToString("00") + ":00";
+        }
+    }   
+}
